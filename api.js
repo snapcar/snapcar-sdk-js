d43c5616d22e089a16b9b78d35c6aa7607835573
Jonathan Tiret <jonathan@snapcar.com>
2015-04-15 14:45:53 +0200

    Updates

---
+++ api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "SnapCar",
+        "SnapCar.APIError",
+        "SnapCar.Address",
+        "SnapCar.BillingDocument",
+        "SnapCar.BillingDocumentTypes",
+        "SnapCar.Booking",
+        "SnapCar.BookingCancellationReasons",
+        "SnapCar.BookingHistory",
+        "SnapCar.BookingPrice",
+        "SnapCar.BookingStatuses",
+        "SnapCar.CancellationFee",
+        "SnapCar.ConfigError",
+        "SnapCar.Driver",
+        "SnapCar.ETAResult",
+        "SnapCar.ETAResultStatuses",
+        "SnapCar.Error",
+        "SnapCar.GeoPoint",
+        "SnapCar.InvalidParametersError",
+        "SnapCar.Location",
+        "SnapCar.MeetingPoint",
+        "SnapCar.PaymentMethod",
+        "SnapCar.Rider",
+        "SnapCar.RiderStatuses",
+        "SnapCar.ServiceClass",
+        "SnapCar.SpecialArea",
+        "SnapCar.SpecialAreaTypes",
+        "SnapCar.TimestampedPoint",
+        "SnapCar.Vehicle"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
